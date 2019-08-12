f0553860338007f321dac816f9533101c94818b6
wangbin <wangbin@icloudportal.com>
2019-08-12 17:32:00 +0800

    导航栏设置

--- source/conf.py
+++ source/conf.py
@@ -21,6 +21,10 @@
 import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_options = {
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+}
 
 from recommonmark.parser import CommonMarkParser
 source_parsers = {
